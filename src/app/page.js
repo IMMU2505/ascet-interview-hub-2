e3e84b81d92ef1fa0ff41983ef5ade7a523f21d0
IMMU2505 <imdadullashaik123456@gmail.com>
2026-05-20 23:50:19 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,12 @@
-"use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { auth } from "@/lib/firebase";
-import { onAuthStateChanged } from "firebase/auth";
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
 
-export default function Home() {
-  const router = useRouter();
+export default async function Home() {
+  const { userId } = auth()
   
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) router.push("/practice");
-      else router.push("/login");
-    });
-  }, [router]);
-
-  return <div className="flex h-screen items-center justify-center text-xl">Loading ASCET Interview Hub...</div>;
+  if (userId) {
+    redirect('/practice')
+  } else {
+    redirect('/sign-in') 
+  }
 }
