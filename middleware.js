82538d3191109c13f7c8912814a197cd88bef411
IMMU2505 <imdadullashaik123456@gmail.com>
2026-05-21 01:03:04 +0530

    Delete middleware.js

--- middleware.js
+++
@@ -1,9 +0,0 @@
-import { authMiddleware } from "@clerk/nextjs"
- 
-export default authMiddleware({
-  publicRoutes: ["/", "/api/check-cap", "/sign-in", "/sign-up"]
-})
- 
-export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"]
-}
