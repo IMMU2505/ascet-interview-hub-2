a081313f066d81961b9517768fb90364536980b5
IMMU2505 <imdadullashaik123456@gmail.com>
2026-05-21 01:05:24 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,14 @@
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'Uber Next Clone',
   description: 'Uber clone with Next.js',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
